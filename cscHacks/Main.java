4523bdf76138ea63d03d287f3632f93c6958d7b1
jebediahtoblerone <xeh606@gmail.com>
2023-10-28 14:11:21 -0400

    main function
    main

--- cscHacks/Main.java
+++ cscHacks/Main.java
@@ -2,5 +2,31 @@ package cscHacks;
 
 public class Main 
 {
+	private static int money = 10000;
+	private static int stepsPerDay = 10;
+	public int count= 0;
+	public boolean dayIsOver= false;
+	public int dayNumber= 0;
+	
+	
+	
+	
+	
+	
+	
+	public void step()
+	{
+		if(dayIsOver)
+		{
+			dayNumber++;
+			count =0;
+		}
+		
+		
+		
+	}
+	
+	
+	
 
 }
