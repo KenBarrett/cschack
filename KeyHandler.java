572dbd2fa12dc1855267876b02b6edbed2f0a4a4
KenBarrett <117616239+KenBarrett@users.noreply.github.com>
2023-10-28 17:36:43 -0400

    Add files via upload

---
+++ KeyHandler.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements Keylistener{
+	
+	public void keyTyped(KeyEvent e){
+	
+	}
+	public void keyPressed(KeyEvent e){
+	
+		int code = e.getKeyCode();
+		
+		if(code == KeyEvent.VK_W)
+		{
+				
+		}
+		if(code == KeyEvent.VK_S)
+		{
+			
+		}
+		if(code == KeyEvent.VK_A)
+		{
+			
+		}
+		if(code == KeyEvent.VK_D)
+		{
+			
+		}
+	
+			
+		
+	}
+	public void keyReleased(KeyEvent e){
+		
+	}
+	
+}
