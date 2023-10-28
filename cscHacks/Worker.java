a1c4c02dc316390d109b2bd90caec6be723d31a4
FierdCS <93808907+FierdCS@users.noreply.github.com>
2023-10-28 16:15:39 -0400

    Update Worker.java

--- cscHacks/Worker.java
+++ cscHacks/Worker.java
@@ -2,15 +2,17 @@ package cscHacks;
 
 public class Worker 
 {
-    public int DessertSpeed = 4;
-    public int MeatSpeed = 4;
-    public int FishSpeed = 4;
-    public int AppsSpeed = 4;
-    public int GarnishSpeed = 4;
-    public boolean isHeadChef = false;
+    private int DessertSpeed = 4;
+    private int MeatSpeed = 4;
+    private int FishSpeed = 4;
+    private int AppsSpeed = 4;
+    private int GarnishSpeed = 4;
+    private boolean isHeadChef = false;
 
 
+    public Worker(){
 
+    }
     public Worker(int joe)
     {
         if(joe==1)
@@ -18,29 +20,60 @@ public class Worker
             this.DessertSpeed=8;
             this.MeatSpeed=8;
             this.FishSpeed=8;
-            this.AppsSpeed =8;
+            this.AppsSpeed=8;
             this.GarnishSpeed=8;
             this.isHeadChef= true;
 
         }
     }
-    public int getDessertSpeed(){
+    public int getDessertSpeed() {
         return DessertSpeed;
     }
-    public int getMeatSpeed(){
+
+    public int getMeatSpeed() {
         return MeatSpeed;
     }
-    public int getFishSpeed(){
-        return MeatSpeed;
+
+    public int getFishSpeed() {
+        return FishSpeed;
     }
-    public int getAppSpeed(){
-        return MeatSpeed;
+
+    public int getAppsSpeed() {
+        return AppsSpeed;
     }
-    public int getGarnishSpeed(){
-        return MeatSpeed;
+
+    public int getGarnishSpeed() {
+        return GarnishSpeed;
     }
-    public boolean isHeadChef(){
+
+    public boolean isHeadChef() {
         return isHeadChef;
     }
+
+    // Setter methods
+    public void setDessertSpeed(int dessertSpeed) {
+        this.DessertSpeed = dessertSpeed;
+    }
+
+    public void setMeatSpeed(int meatSpeed) {
+        this.MeatSpeed = meatSpeed;
+    }
+
+    public void setFishSpeed(int fishSpeed) {
+        this.FishSpeed = fishSpeed;
+    }
+
+    public void setAppsSpeed(int appsSpeed) {
+        this.AppsSpeed = appsSpeed;
+    }
+
+    public void setGarnishSpeed(int garnishSpeed) {
+        this.GarnishSpeed = garnishSpeed;
+    }
+
+    public void setHeadChef(boolean isHeadChef) {
+        this.isHeadChef = isHeadChef;
+    }
+    
 
 }
